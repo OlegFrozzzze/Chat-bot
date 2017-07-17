3a9d418cb323cc8ac0a293f5b65430958e4c6e7e
Frozzzze <ji900@yandex.ru>
2017-07-17 13:12:39 +0300

    Создал несколько объектов

--- src/ChatBotMain.java
+++ src/ChatBotMain.java
@@ -1,8 +1,25 @@
-/**
- * Created by ОЛЕГ on 17.07.2017.
- */
-public class ChatBotMain {
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+class SimpleChatBot extends JFrame {
+
+    final String TITLE_OF_PROGRAM = "Chatter: simple chatbot";
+    final int START_LOCATION = 200;
+    final int WINDOW_WIDTH = 350;
+    final int WINDOW_HEIGHT = 450;
+
+    JTextPane dialogue; // area for dialog
+    JCheckBox ai;       // enable/disable AI
+    JTextField message; // field for entering messages
+   // SimpleBot sbot;     // chat-bot class (in bot package)
+
+
     public static void main(String[] args) {
-        
+        new SimpleChatBot();
+    }
+    
+    SimpleChatBot(){
+
     }
 }
